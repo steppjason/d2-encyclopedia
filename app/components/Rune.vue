1a3473c23fad160235a81fa3c8cd7988a5d7e673
Jason Stepp <hi@jasonstepp.me>
2026-02-26 22:55:47 -0700

    runes

---
+++ app/components/Rune.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <img :src="src" :alt="type" />
+  </div>
+</template>
+<script setup lang="ts">
+const { type } = defineProps<{
+  type: Rune
+}>()
+
+const src = `/runes/${type.toLowerCase()}.webp`
+
+type Rune =
+  | 'El'
+  | 'Eld'
+  | 'Tir'
+  | 'Nef'
+  | 'Eth'
+  | 'Ith'
+  | 'Tal'
+  | 'Ral'
+  | 'Ort'
+  | 'Thul'
+  | 'Amn'
+  | 'Sol'
+  | 'Shael'
+  | 'Dol'
+  | 'Hel'
+  | 'Io'
+  | 'Lum'
+  | 'Ko'
+  | 'Fal'
+  | 'Lem'
+  | 'Pul'
+  | 'Um'
+  | 'Mal'
+  | 'Ist'
+  | 'Gul'
+  | 'Vex'
+  | 'Ohm'
+  | 'Lo'
+  | 'Sur'
+  | 'Ber'
+  | 'Jah'
+  | 'Cham'
+  | 'Zod'
+</script>
