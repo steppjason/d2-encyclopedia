b4ec69c4c4b7ed73927cbf07a09eeb0f8524daf6
Jason Stepp <hi@jasonstepp.me>
2026-02-26 22:55:36 -0700

    turn off some eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,10 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt()
+export default withNuxt({
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'vue/multi-word-component-names': 'off'
+  }
+})
